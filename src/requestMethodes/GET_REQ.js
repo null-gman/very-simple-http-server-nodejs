6f48fd9595c21950bb73fcaf7b4138b5c60e66e5
gman <omar5mahmoud3ahmed6@gmail.com>
2025-04-04 14:02:49 +0200

    new fetchers and improvment includingd :: now the server can handle larg files by fixing 'isFileExist' 'getFileSizeByte'
    # but also it not tested well git add .

--- src/requestMethodes/GET_REQ.js
+++ src/requestMethodes/GET_REQ.js
@@ -1,35 +1,64 @@
-
 const fsModules = require("../fsModules/fsModules.js");
 const  setMIMEtype = require("../utils/setMIME")
+const utils = require("../utils/utils.js")
+
+
+
+/*
+  i use this function to get the file the requsted and it path 
+
+*
+/
+// getDirAndFileNameFromFileFullPath("/folder/folder/folder/folder/") /* gives bug */
+function getDirAndFileNameFromFileFullPath(filePath)
+{ 
+  const pathArray = utils.removeValueFromArray(filePath.split("/"),"");
+  let file = "",dir = "";
+
+  if(pathArray.length === 0)
+    return ["/",""];
+  else if(pathArray.length === 1)
+    return ["/",pathArray[0]];
+
+  for (let index = 0; index < pathArray.length - 1; index++) 
+    dir += "/" + pathArray[index];
+  
+  
+  file = pathArray.at(-1);
+  return [dir,file];
+}
+
 async function GET_REQ(req,res,rootDir) {
     if (!req | !res | !rootDir) throw new Error("can't be null");
 
-    const REQ_URL = req.url;
-    const isFileExist = await fsModules.isFileExistAsync(
-      rootDir + REQ_URL
-    );
 
+    const [reqPath,reqFile] = getDirAndFileNameFromFileFullPath(req.url);
+
+    const REQ_URL = req.url;
     let resFilePath = "";
-    if (isFileExist) {
-      resFilePath = rootDir + REQ_URL;
-      res.statusCode = 200;
-    } else if(REQ_URL === "/" && await fsModules.isFileExistAsync(rootDir + "/index.html")) {
+  
+
+    if(REQ_URL === "/" && await fsModules.isFileExistAsync(rootDir , "index.html")) {
         resFilePath = rootDir + "/index.html";
         res.statusCode = 200;
+      }
+    else if( await fsModules.isFileExistAsync(rootDir +"/"+reqPath, reqFile)){
+      resFilePath = rootDir + req.url;
+      res.statusCode = 200;
     }
     else{
-      if (await  fsModules.isFileExistAsync(rootDir + "/404.html")){
+      res.statusCode = 404;  
+      if (await fsModules.isFileExistAsync(rootDir , "404.html")){
         resFilePath = rootDir + "/404.html";
-        res.statusCode = 404;  
       }else{
         res.end("404 not found");
         return;
       }
       
     }
-
+    
     const { MIME } = getFileInfo(resFilePath);
-    const FileSize = fsModules.getFileSizeBytes(resFilePath);
+    const FileSize = await fsModules.getFileSizeBytesAsync(resFilePath);
     res.setHeader("Content-Length", FileSize);
     res.setHeader("content-type", MIME);
     function writeRes(data) {
@@ -44,5 +73,7 @@ function getFileInfo(filePath) {
     const MIME = setMIMEtype(FILE);
     return { MIME };
 }
-  
+
+
+
 module.exports = GET_REQ;
